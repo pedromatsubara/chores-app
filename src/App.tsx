54f34225de6e2be8a0eb2baaad6380c72793f7f7
fuzarin <joaopedrofuzaro9@gmail.com>
2024-11-29 00:38:20 -0300

    feat(router): Create router to profile page and user list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import ProfilePage from "./pages/ProfilePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProfilePage from "./pages/profilePage/ProfilePage";
+import UsersPage from "./pages/users/UsersPage";
 
 const App: React.FC = () => {
-
   return (
-    <div>
-      <ProfilePage />
-    </div>
+    <Router>
+      <Routes>
+        {/* Página de perfil */}
+        <Route path="/users" element={<UsersPage />} />
+      </Routes>
+      <Routes>
+        {/* Página de perfil */}
+        <Route path="/profile/:id" element={<ProfilePage />} />
+      </Routes>
+    </Router>
   );
 };
 
